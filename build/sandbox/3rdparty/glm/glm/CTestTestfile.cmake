9df49f5dfe20fef9c4238381e53ff38d59cea77d
M8te0r-laptop <602578045@qq.com>
2025-09-09 08:58:23 +0800

    update gitignore

--- build/sandbox/3rdparty/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/crystalized/cpp_project/mySandbox/sandbox/3rdparty/glm/glm
-# Build directory: /Users/crystalized/cpp_project/mySandbox/build/sandbox/3rdparty/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
